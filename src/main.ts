fdc231e0afd25fcf9a69e8a37078493d62118538
izaelgs <izael-123@outlook.com>
2025-02-04 19:35:44 -0300

    Configure dynamic port binding using environment variable

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
